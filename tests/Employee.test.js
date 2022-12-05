c421a09e2d396808222ae65b8ffd56c49a543edf
mintedd <xxxxiheartyouxxxxx@gmail.com>
2022-12-04 22:21:45 -0500

    was in the wrong repo, started code for the diff roles, template for html with bootstrap

---
+++ tests/Employee.test.js
@@ -0,0 +1,13 @@
+const Employee = require('../lib/Employee')
+
+
+describe("Engineer", () => {
+    describe("something", () => () {
+        it("", () => {
+            const obj = new Engineer();
+
+            expect("",).toEqual()
+        })
+    })
+
+})
